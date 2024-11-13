f778d832c05323c370d3d01cba710e29fec3da7c
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-13 13:13:03 +0200

    create the dao for operation on the database

---
+++ app/src/main/java/com/example/runningapp/db/RunDao.kt
@@ -0,0 +1,46 @@
+package com.example.runningapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RunDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNewRun(runDto: RunDto)
+
+    @Delete
+    suspend fun deleteRun(runDto: RunDto)
+
+    @Query("SELECT * FROM running_table ORDER BY timeStamp DESC ")
+    fun getAllRunsSortedByDate():LiveData<RunDto>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC ")
+    fun getAllRunsSortedByInMillis():LiveData<RunDto>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC ")
+    fun getAllRunsSortedByAvgSpeed():LiveData<RunDto>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC ")
+    fun getAllRunsSortedByCaloriesBurned():LiveData<RunDto>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC ")
+    fun getAllRunsSortedByDistance():LiveData<RunDto>
+
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis():LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned():LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance():LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed():LiveData<Float>
+
+}

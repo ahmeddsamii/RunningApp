1fb2a63bd6fba8da09aa3f0e53a74e30867a676c
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-14 18:23:54 +0200

    added some Constants

--- app/src/main/java/com/example/runningapp/other/Constants.kt
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -3,8 +3,17 @@ package com.example.runningapp.other
 object Constants {
     const val RUNNING_DATABASE_NAME = "Running_database_name"
     const val LOCATION_PERMISSION_CODE = 123
+     const val NOTIFICATION_PERMISSION_REQUEST_CODE = 321
 
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+    const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_id"
+    const val NOTIFICATION_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
 }

0b087351d6ed928320c09f99fd559841bbf0dbe4
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-14 11:05:20 +0200

    implementing the map view lifecycle and initial setup

--- app/src/main/java/com/example/runningapp/ui/viewmodels/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodels/fragments/TrackingFragment.kt
@@ -7,17 +7,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
+import com.example.runningapp.databinding.FragmentTrackingBinding
 import com.example.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
+    private lateinit var binding:FragmentTrackingBinding
+    private var map:GoogleMap? = null
     private val viewModel: MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+        binding = FragmentTrackingBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
     }
 }

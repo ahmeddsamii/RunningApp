5f2895da14c0ba88cf7eed8ec5512c5d83bfb853
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-14 11:36:03 +0200

    added commands in form of Constants

--- app/src/main/java/com/example/runningapp/other/Constants.kt
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -3,4 +3,8 @@ package com.example.runningapp.other
 object Constants {
     const val RUNNING_DATABASE_NAME = "Running_database_name"
     const val LOCATION_PERMISSION_CODE = 123
+
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
 }

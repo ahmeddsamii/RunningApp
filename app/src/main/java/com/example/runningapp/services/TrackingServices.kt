34372f65ef658ab8c0006e16d1721b18a22793b4
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-14 18:41:26 +0200

    implementing the whole logic of location and receiving the new location

--- app/src/main/java/com/example/runningapp/services/TrackingServices.kt
+++ app/src/main/java/com/example/runningapp/services/TrackingServices.kt
@@ -1,28 +1,199 @@
 package com.example.runningapp.services
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_MUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.os.Build
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import com.example.runningapp.MainActivity
+import com.example.runningapp.R
 import com.example.runningapp.other.Constants.ACTION_PAUSE_SERVICE
+import com.example.runningapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningapp.other.Constants.ACTION_STOP_SERVICE
+import com.example.runningapp.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.example.runningapp.other.Constants.LOCATION_UPDATE_INTERVAL
+import com.example.runningapp.other.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.runningapp.other.Constants.NOTIFICATION_ID
+import com.example.runningapp.other.Constants.NOTIFICATION_NAME
+import com.example.runningapp.other.TrackingUtility
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.FusedOrientationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
-class TrackingServices: LifecycleService() {
+
+typealias polyLine = MutableList<LatLng>
+typealias polyLines = MutableList<polyLine>
+
+class TrackingServices : LifecycleService() {
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<polyLines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.value = false
+        pathPoints.value = mutableListOf()
+    }
+
+    private var isFirstTimeToStartService = true
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        isTracking.observe(this){
+            updateLocationTracking(it)
+        }
+    }
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when(intent.action){
-                ACTION_START_OR_RESUME_SERVICE ->{
-                    Timber.d("started or resume service")
+            when (intent.action) {
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    if (isFirstTimeToStartService) {
+                        Timber.d("Starting foreground service")
+                        foregroundService()
+                        isFirstTimeToStartService = false
+                    } else {
+                        Timber.d("resuming service...")
+                    }
                 }
-                ACTION_PAUSE_SERVICE ->{
+
+                ACTION_PAUSE_SERVICE -> {
                     Timber.d("paused service")
                 }
-                ACTION_STOP_SERVICE ->{
+
+                ACTION_STOP_SERVICE -> {
                     Timber.d("stopped service")
                 }
             }
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        }else{
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+
+    //need to add empty list of lat long because ->
+    //-> when user click stop we need to add empty line on map then start the original line again
+    private fun addEmptyPolyLine() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.value = this
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result.locations.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION ${location.latitude} ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.value = this
+            }
+        }
+    }
+
+
+    private fun foregroundService() {
+        //this line is because when we start after pause to draw nothing then continue
+        addEmptyPolyLine()
+
+        isTracking.value = true
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setCategory(NotificationCompat.CATEGORY_SERVICE)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setShowWhen(false)
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val notificationChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT or FLAG_MUTABLE
+    )
+
 }

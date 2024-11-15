3439b4ed7c20131d0c6a6fca5fe8cff8d7112873
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-15 10:18:14 +0200

    added function here to get the time formatted as string

--- app/src/main/java/com/example/runningapp/other/TrackingUtility.kt
+++ app/src/main/java/com/example/runningapp/other/TrackingUtility.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.Context
 import android.os.Build
 import pub.devrel.easypermissions.EasyPermissions
+import java.sql.Time
+import java.util.concurrent.TimeUnit
 
 object TrackingUtility {
     fun hasLocationPermissions(context: Context): Boolean {
@@ -26,4 +28,30 @@ object TrackingUtility {
 
         return hasForegroundPermissions && hasBackgroundPermission
     }
+
+
+
+
+
+    fun getFormattedStopWatchTime(ms:Long, includeMills:Boolean = false):String{
+        var milliseconds = ms
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+        milliseconds -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+        milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+        if (!includeMills){
+            return "${if (hours < 10) "0" else ""}$hours:" +
+                    "${if (minutes < 10) "0" else ""}$minutes:"+
+                    "${if (seconds < 10) "0" else ""}$seconds"
+        }
+        milliseconds -= TimeUnit.SECONDS.toMillis(seconds)
+        milliseconds /= 10
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:"+
+                "${if (seconds < 10) "0" else ""}$seconds:" +
+                "${if (milliseconds < 10) "0" else ""}$milliseconds"
+
+    }
+
 }

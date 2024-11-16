ef59eda062d277b41bee52aa2f66ea262591b36d
ahmbd <ahmbdelkhalek@gmail.com>
2024-11-16 13:11:39 +0200

    added subscribeToObservers function which is responsible for setting data on the ui

--- app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.runningapp.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,18 +8,58 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.runningapp.R
+import com.example.runningapp.databinding.FragmentStatisticsBinding
+import com.example.runningapp.other.TrackingUtility
 import com.example.runningapp.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment() {
+    private lateinit var binding:FragmentStatisticsBinding
     private val viewModel: StatisticsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false)
+        binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun subscribeToObservers(){
+        viewModel.totalTimeRun.observe(viewLifecycleOwner){
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                binding.tvTotalTime.text = totalTimeRun
+            }
+        }
+
+        viewModel.totalDistance.observe(viewLifecycleOwner){
+            it?.let {
+                val distanceInKm =  it / 1000f
+                binding.tvTotalDistance.text = "${(round(distanceInKm * 10f ) / 10f)}km"
+            }
+        }
+
+        viewModel.totalSpeed.observe(viewLifecycleOwner){
+            it?.let {
+                val avgSpeedInKm = round(it * 10f) / 10f
+                binding.tvAverageSpeed.text = "${avgSpeedInKm}km/h"
+            }
+        }
+
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner){
+            it?.let {
+                binding.tvTotalCalories.text = "${it}kcal"
+            }
+        }
     }
 }
